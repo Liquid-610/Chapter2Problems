92d76623dd2a3afa6323348e8138aacdbb87a6a6
Liquid-610 <jamesbarnes71@gmail.com>
2014-02-19 10:11:02 -0500

    hours

---
+++ Room.cs/Hours/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes;
+            int time;
+            int hours;
+            int
+                
+        }
+    }
+}
