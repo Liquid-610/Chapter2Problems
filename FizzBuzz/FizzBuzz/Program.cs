732f2c72f9bb2c7611ccbbfbeb52617b2d4245e6
Liquid-610 <jamesbarnes71@gmail.com>
2014-02-21 10:10:28 -0500

    Fizz Buzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+
+            for (i = 0; i <= 101; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+
+
+
+            }
+        }
+    }
+}
