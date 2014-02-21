d1faf0c3cfe399a320e87cd3f3d2d6f74f2012fc
Liquid-610 <jamesbarnes71@gmail.com>
2014-02-21 09:39:49 -0500

    Payroll

---
+++ Room.cs/Payroll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input;
+            Console.Write("Enter your name:"); input = Console.ReadLine();
+
+            Console.Write("Social Security number:"); input = Console.ReadLine();
+
+            Console.Write("Hourly pay rate:"); input = Console.ReadLine();
+
+            Console.Write("Hours worked:"); input = Console.ReadLine();
+        }
+    }
+}
